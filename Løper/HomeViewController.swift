3c5cc74f983eb69f5977e2158f2e92d58f769cf4
calebrudnicki <calebrudnicki@gmail.com>
2016-07-12 17:10:42 -0400

    Home page makeover
    Functionality added: start run button now connects to the
    RunTrackerViewController after an alert controller
    
    Design added: messed with colors, icons, and font sizes on the
    HomeViewController; visible map is also not interactive

--- Løper/HomeViewController.swift
+++ Løper/HomeViewController.swift
@@ -5,12 +5,14 @@
 //  Created by Caleb Rudnicki on 7/11/16.
 //  Copyright © 2016 Caleb Rudnicki. All rights reserved.
 //
+//  Reverse runner icon in top left created by Freepik at http://www.freepik.com
+//
 
 import UIKit
 import MapKit
 import CoreLocation
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
 
 //MARK: Outlets
     
@@ -19,16 +21,30 @@ class HomeViewController: UIViewController {
     
 //MARK: Variables
     
-    var coreLocationManager = CLLocationManager()
+    var locationManager = CLLocationManager()
     
     
 //MARK: viewDidLoad()
     
+    //This function loads the view and sets up the location manager settings
     override func viewDidLoad() {
         super.viewDidLoad()
-        if CLLocationManager.locationServicesEnabled() {
-            coreLocationManager.startUpdatingLocation()
-        }
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        mapView.tintColor = UIColor(red: 0.44, green: 0.62, blue: 0.80, alpha: 1.0)
+    }
+    
+    
+//MARK: viewDidAppear()
+    
+    //This function disables the map features before checking for authorization status
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        mapView.zoomEnabled = false
+        mapView.scrollEnabled = false
+        mapView.rotateEnabled = false
+        self.checkLocationAuthorizationStatus()
     }
     
     
@@ -39,13 +55,62 @@ class HomeViewController: UIViewController {
     }
     
     
+//MARK: Style Functions
+    
+    //This function sets the status bar to white
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    
 //MARK: Actions
     
+    //This function for the run button activates an alert controller that either segues into the next view controller or returns back to the home screen
     @IBAction func runButtonTapped(sender: AnyObject) {
-        print("run button selected")
+        let alertController = UIAlertController(title: nil, message: "Are you sure you're ready to start your run. If you proceed the timer will begin?", preferredStyle: .ActionSheet)
+        let yesAction = UIAlertAction(title: "Yes, I am ready", style: .Default) { (action) in
+            self.performSegueWithIdentifier("startRunSegue", sender: self)
+        }
+        let noAction = UIAlertAction(title: "No wait, I need a minute", style: .Cancel, handler: nil)
+        alertController.addAction(yesAction)
+        alertController.addAction(noAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
     @IBAction func pastRunsButtonTapped(sender: AnyObject) {
-        print("past runs button selected")
+        //To be edited at a later date
     }
+    
+    
+//MARK: Map Settings
+    
+    //This functions checks to see the users authorization status and either allows the app to acces their location or sends an authorization request
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    //This function grabs the user's location, makes a 2D coordinate out of it, and then sets the view of the map onto that coordinate
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0]
+        let coord2D = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let coordinateRegion = MKCoordinateRegion(center: coord2D, span: MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003))
+        mapView.setRegion(coordinateRegion, animated: false)
+    }
+    
+    
+//MARK: Segues
+    
+    //This function recognizes the segue called in code based on the user's action and performs the correct segue
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if identifier == "startRunSegue" {
+                let runTrackerViewController = segue.destinationViewController as! RunTrackerViewController
+            }
+        }
+    }
+    
 }
---
+++ Løper/RunTrackerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RunTrackerViewController.swift
+//  Løper
+//
+//  Created by Caleb Rudnicki on 7/12/16.
+//  Copyright © 2016 Caleb Rudnicki. All rights reserved.
+//
+
+import UIKit
+
+class RunTrackerViewController: UIViewController {
+
+//MARK: viewDidLoad()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+//MARK: didReceiveMemoryWarning()
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
